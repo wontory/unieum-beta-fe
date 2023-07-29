dad342fa7fe1c3ae2d531ceb428fe831c6495124
조성원 <tjddnjsjo@kakao.com>
2023-07-29 16:09:37 +0900

    feat: 랜딩 페이지 추가

--- src/components/UI/Card/Card.jsx
+++ src/components/UI/Card/Card.jsx
@@ -1,9 +1,10 @@
-const Card = ({ className, children }) => {
+const Card = ({ className, align, children }) => {
   const classes = "card card-bordered bg-base-100 " + className;
+  const body_classes = "card-body p-6 " + align;
 
   return (
     <div className={classes}>
-      <div className="card-body p-6">{children}</div>
+      <div className={body_classes}>{children}</div>
     </div>
   );
 };
