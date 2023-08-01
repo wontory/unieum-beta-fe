edf1d6cbc258722e50ace4b406098a48a70c1b2d
조성원 <tjddnjsjo@kakao.com>
2023-08-01 16:30:13 +0900

    feat: MultipleChoice 유형 추가

--- src/components/Question/MultipleChoice.jsx
+++ src/components/Question/MultipleChoice.jsx
@@ -1,3 +1,30 @@
-const MultipleChoice = () => {};
+import { useContext } from "react";
+
+import StepContext from "../../stores/step-context";
+
+import Card from "../UI/Card/Card";
+
+const MultipleChoice = ({ title, options }) => {
+  const ctx = useContext(StepContext);
+
+  return (
+    <Card align="items-center">
+      <h2 className="card-title">{title}</h2>
+      <div className="flex gap-2">
+        {options.map((option, index) => (
+          <div className="flex flex-col items-center gap-2 w-full">
+            <Card className="h-full">{option}</Card>
+            <input type="radio" name={"radio-" + index} className="radio" />
+          </div>
+        ))}
+      </div>
+      <div className="card-actions justify-end">
+        <button className="btn btn-primary" onClick={ctx.onClickNext}>
+          셋 다 별로에요
+        </button>
+      </div>
+    </Card>
+  );
+};
 
 export default MultipleChoice;
