1d01a95adcf619cabf4f9731a048916f8d8da9f8
조성원 <tjddnjsjo@kakao.com>
2023-08-01 16:30:20 +0900

    feat: Essay 유형 추가

--- src/components/Question/Essay.jsx
+++ src/components/Question/Essay.jsx
@@ -1,3 +1,26 @@
-const Essay = () => {};
+import { useContext } from "react";
+
+import StepContext from "../../stores/step-context";
+
+import Card from "../UI/Card/Card";
+
+const Essay = ({ title, placeholder }) => {
+  const ctx = useContext(StepContext);
+
+  return (
+    <Card align="items-center">
+      <h2 className="card-title">{title}</h2>
+      <textarea
+        className="textarea textarea-bordered w-full"
+        placeholder={placeholder}
+      ></textarea>
+      <div className="card-actions justify-end">
+        <button className="btn btn-primary" onClick={ctx.onClickNext}>
+          다음 질문
+        </button>
+      </div>
+    </Card>
+  );
+};
 
 export default Essay;
