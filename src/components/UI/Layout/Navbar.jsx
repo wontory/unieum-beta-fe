f3a061afeae415b944a64d63be406cecbdd74fee
조성원 <tjddnjsjo@kakao.com>
2023-08-02 10:14:01 +0900

    fix: 홈버튼 오류 수정

--- src/components/UI/Layout/Navbar.jsx
+++ src/components/UI/Layout/Navbar.jsx
@@ -1,4 +1,8 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+
+import StepContext from "../../../stores/step-context";
 
 import { CgMenu } from "react-icons/cg";
 
@@ -6,14 +10,25 @@ import { ReactComponent as Icon } from "../../../assets/images/unieum_icon.svg";
 import { ReactComponent as Logo } from "../../../assets/images/unieum_logo.svg";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const ctx = useContext(StepContext);
+
+  const handleClickHome = () => {
+    ctx.onClickHome();
+    navigate("/");
+  };
+
   return (
     <nav className="navbar max-w-[1200px] w-full">
       <div className="flex-1">
-        <Link className="btn btn-ghost normal-case text-xl" to="/">
+        <button
+          className="btn btn-ghost normal-case text-xl"
+          onClick={handleClickHome}
+        >
           <Icon />
           <Logo width="48" />
           <div className="badge badge-primary">Beta</div>
-        </Link>
+        </button>
       </div>
       <div className="flex-none gap-2">
         <Link
