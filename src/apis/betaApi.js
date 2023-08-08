018920657da5c506f8e25e18f9c0cc33ba39b1f2
조성원 <tjddnjsjo@kakao.com>
2023-08-08 16:16:44 +0900

    feat: API 추가

---
+++ src/apis/betaApi.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const ROUTE = "beta";
+
+const betaInstance = axios.create({
+  baseURL: "https://develop.unieum.kr:4000/api/admin",
+  headers: {
+    AccessControlAllowOrigin: "*",
+  },
+  withCredentials: true,
+});
+
+const post3TestGeneration = async (formData) => {
+  return await betaInstance.post(`${ROUTE}/3-test-generation`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
+
+const postSurvey = async (answer) => {
+  return await betaInstance.post(`${ROUTE}/survey`, answer, {
+    headers: { "Content-Type": "application/json" },
+  });
+};
+
+export const betaApi = {
+  post3TestGeneration,
+  postSurvey,
+};
